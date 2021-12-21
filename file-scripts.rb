8112c66d313131eff83d784877f3ccee6e23486f
Eric Torres <erictorres4@protonmail.com>
2021-12-21 00:53:44 -0800

    Revert "Update file permissions"
    This reverts commit 379086661145f059974b94ea4503d8331195d6a5.

--- file-scripts.rb
+++ file-scripts.rb
@@ -1,10 +1,10 @@
 class HelperScripts < Formula
   desc "Various scripts for performing file-related operations such as editing and deleting"
-  homepage "https://github.com/etorres4/file-scripts"
+  homepage "https://github.com/etorres4/helper-scripts"
   url "https://github.com/etorres4/file-scripts",
     :using => :git
   sha256 "7b6c3f363e3b34787765b5975d87f861a19eeda278612cbf8c5176e3e2732cd9"
-  version "0.9.1"
+  version "0.9"
 
   # No build dependencies
   bottle :unneeded
