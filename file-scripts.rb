0eb55ccce0f2ece76f910ba63e483c1440cf6688
Eric Torres <erictorres4@protonmail.com>
2022-01-10 11:09:34 -0800

    Fix packaging for homebrew

--- file-scripts.rb
+++ file-scripts.rb
@@ -1,21 +1,18 @@
-class HelperScripts < Formula
+class FileScripts < Formula
   desc "Various scripts for performing file-related operations such as editing and deleting"
-  homepage "https://github.com/etorres4/helper-scripts"
+  homepage "https://github.com/etorres4/file-scripts"
   url "https://github.com/etorres4/file-scripts",
     :using => :git
-  sha256 "7b6c3f363e3b34787765b5975d87f861a19eeda278612cbf8c5176e3e2732cd9"
   version "0.9.2"
 
-  # No build dependencies
-  bottle :unneeded
+  depends_on "python@3.10"
+  depends_on "fd"
+  depends_on "fzf"
 
   def install
-    #bin.install "ddusb.py" => "ddusb"
-    #bin.install "fless.sh" => "fless"
-    #bin.install "lsgroups.sh" => "lsgroups"
-    #bin.install "lsusers.sh" => "lsusers"
+    system Formula["python@3.10"].opt_bin/"python3", *Language::Python.setup_install_args(prefix)
 
     # Install completions to zsh/site-functions
-    zsh_completion.install Dir["zsh/completions/_*"]
+    zsh_completion.install Dir["zsh/_*"]
   end
 end
