607a7d488b2fd97ba7399c966cf51877012a41c7
Eric Torres <erictorres4@protonmail.com>
2022-09-28 22:05:05 -0700

    Update package versions on packaging scripts

--- file-scripts.rb
+++ file-scripts.rb
@@ -2,15 +2,16 @@ class FileScripts < Formula
   desc "Various scripts for performing file-related operations"
   homepage "https://github.com/etorres4/file-scripts"
   url "https://github.com/etorres4/file-scripts", :using => :git
-  version "1.1.3"
+  version "1.1.4"
   sha256 "fad7da96c72c8bef81f3ec6a2d1cb13b09fb44e084f98b05ba3a7dfd8b41ae12"
 
   depends_on "fd"
   depends_on "fzf"
-  depends_on "python@3.10"
+  #depends_on "python@3.10"
 
   def install
     system Formula["python@3.10"].opt_bin/"python3", *Language::Python.setup_install_args(prefix)
+    system 
 
     # Install completions to zsh/site-functions
     zsh_completion.install Dir["zsh/_*"]
